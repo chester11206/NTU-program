b95d379b43772573edc9b899676e15cc6d809361
chester11206 <b04705039@ntu.edu.tw>
2016-11-08 13:29:55 +0800

    2016.11.08 0129

---
+++ R/firstR.r
@@ -0,0 +1,30 @@
+rm(list=ls(all.names=TRUE))
+library(XML)
+library(RCurl)
+library(httr)
+Sys.setlocale(category = "LC_ALL", locale = "cht")
+startNo = 4637
+endNo   = 4641 
+subPath <- "https://www.ptt.cc/bbs/movie/index"
+alldata = data.frame()
+for( pid in startNo:endNo )
+{
+  urlPath <- paste(subPath, pid, ".html", sep='')
+  temp    <- getURL(urlPath, encoding = "big5")
+  xmldoc  <- htmlParse(temp)
+  title   <- xpathSApply(xmldoc, "//div[@class=\"title\"]", xmlValue)
+  title   <- gsub("\n", "", title)
+  title   <- gsub("\t", "", title)
+  path    <- xpathSApply(xmldoc, "//div[@class='title']/a//@href")
+  Erroresult<- tryCatch({
+    subdata <- data.frame(title, path)
+    alldata <- rbind(alldata, subdata)
+  }, warning = function(war) {
+    print(paste("MY_WARNING:  ", urlPath))
+  }, error = function(err) {
+    print(paste("MY_ERROR:  ", urlPath))
+  }, finally = {
+    print(paste("End Try&Catch", urlPath))
+  })
+}
+write.table(alldata, file = "movie.csv")
